e55b08740fe8a360188d958ee50c85affd3985f1
Tommy Grullon <tommycontreras34@gmail.com>
2026-05-21 16:21:01 -0400

    in progress: add new article form and implement routing for article management

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from "express"
+import path from "path"
+import { fileURLToPath } from "url"
 
 const app = express()
 
@@ -7,3 +9,26 @@ app.use(express.static("public"))
 app.listen(3000, () => {
     console.log("Running on port 3000")
 })
+
+const reRenderPage = (page) => {
+    const __filename = fileURLToPath(import.meta.url)
+    const __dirname = path.dirname(__filename)
+
+    return path.join(__dirname, page)
+}
+
+app.get("/", (req, res) => {
+    res.sendFile(reRenderPage("index.html"))
+})
+
+app.get("/article/add", (req, res) => {
+    res.sendFile(reRenderPage("/article/new.html"))
+})
+
+app.post("/article/add", (req, res) => {
+    console.log(req.body)
+})
+
+// addArticleToJson = async (data) => {
+//     await fs.writeFile(ARTICLE_JSON, JSON.stringify(data, null, 2), "utf8")
+// }
